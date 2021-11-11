eddf06dd2765dd460e8d9c5dd5d6056e833fb00a
unknown <rryanwilsonw@gmail.com>
2021-11-11 17:09:55 -0500

    Function created to calculate a persons bmi

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("hello world");
+// Funtion to calculate the BMI of a person based off of wieght in kg and height in meters.
+function calculateBmi(weight, height) {
+	let Bmi = Math.floor(weight / (height * height));
+	console.log(Bmi);
+}
+
+calculateBmi(75, 1.8);
